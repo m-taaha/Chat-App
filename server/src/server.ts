b5cd60236e6b25687bd911c48037bfe5974838ea
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-21 22:55:05 +0530

    feat: set web socket server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,25 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
 let userCount = 0;
+let allSockets: WebSocket[] = []; //all socket connections global variable
 
 wss.on("connection", (socket) => {
-    userCount = userCount++;
+    allSockets.push(socket); //pushing this socket to global sockets in order to broadcast
+
+
+    userCount = userCount + 1;
     console.log("user connected #" + userCount);
+
+    socket.on("message", (message) => {
+        console.log(`message recieved ${message.toString()} from user: ${userCount}`);
+
+        for (const s of allSockets){
+        setTimeout(() => {
+            s.send(`${message.toString()} : sent from the server`)
+        }, 1000)
+    }
+
+    })
 })
