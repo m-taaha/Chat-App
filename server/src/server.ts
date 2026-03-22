f7a338139f8bf7896e4f13d767877bd78fe39636
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-22 21:14:54 +0530

    refactor: refine the logic of chat server

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ const wss = new WebSocketServer({port: 8080});
 interface User {
     socket: WebSocket;
     room: string;
+    username: string;
 }
 
 
@@ -15,37 +16,45 @@ wss.on("connection", (socket) => {
 
 
     socket.on("message", (message: string) => {
-        const parsedMessage = JSON.parse(message);
+        const parsedMessage = JSON.parse(message.toString());
 
-        if(parsedMessage.type == "join") {
-            console.log(`user joined room ${parsedMessage.payload.roomId}`)
+        if(parsedMessage.type === "join") {
+
+            const alreadyJoined = allSockets.find(s => s.socket === socket);
+            if(!alreadyJoined) {
             allSockets.push({
                 socket,
-                room: parsedMessage.payload.roomId
-            })
+                room: parsedMessage.payload.roomId,
+                username: parsedMessage.payload.username,
+            });
         }
+    }
 
 
-        if(parsedMessage.type == "chat") {
+        if(parsedMessage.type === "chat") {
             console.log("user want to chat");
             // const currentUserRoom = allSockets.filter(s => s.socket === socket)
-            let currentUserRoom = null;
-            for (let i = 0; i < allSockets.length; i++) {
-                if(allSockets[i]?.socket == socket) {
-                currentUserRoom = allSockets[i]?.room
-            }
-        }
-        
-         for (let i = 0; i < allSockets.length; i++) {
-           if (allSockets[i]?.room == currentUserRoom) {
-             allSockets[i]?.socket.send(parsedMessage.payload.message);
-           }
-         }
-        }
+            // let currentUserRoom = null;
 
-       
+            const currentUser = allSockets.find(s => s.socket === socket);
+            if(!currentUser) return;
+
+            allSockets
+            .filter(s => s.room === currentUser.room)
+            .forEach(s => {
+                s.socket.send(JSON.stringify({
+                    username: currentUser.username,
+                    message: parsedMessage.payload.message
+                }));
+            });
+        }
+    });
+
+    // cleanup on disconnect - 
+    socket.on("close", () => {
+        allSockets = allSockets.filter(s => s.socket !== socket);
+        console.log("User disconnected, clean up")
     })
 
 
-   
     })
