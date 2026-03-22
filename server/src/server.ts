d3d45c14f533264a351766059388b497b21c9f31
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-22 10:37:44 +0530

    feat: add room connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,24 +2,50 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
-let userCount = 0;
-let allSockets: WebSocket[] = []; //all socket connections global variable
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+
+let allSockets: User[] = []; //all socket connections global variable - (multiple sockts)
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket); //pushing this socket to global sockets in order to broadcast
 
 
-    userCount = userCount + 1;
-    console.log("user connected #" + userCount);
 
-    socket.on("message", (message) => {
-        console.log(`message recieved ${message.toString()} from user: ${userCount}`);
+    socket.on("message", (message: string) => {
+        const parsedMessage = JSON.parse(message);
 
-        for (const s of allSockets){
-        setTimeout(() => {
-            s.send(`${message.toString()} : sent from the server`)
-        }, 1000)
-    }
+        if(parsedMessage.type == "join") {
+            console.log(`user joined room ${parsedMessage.payload.roomId}`)
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
 
+
+        if(parsedMessage.type == "chat") {
+            console.log("user want to chat");
+            // const currentUserRoom = allSockets.filter(s => s.socket === socket)
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if(allSockets[i]?.socket == socket) {
+                currentUserRoom = allSockets[i]?.room
+            }
+        }
+        
+         for (let i = 0; i < allSockets.length; i++) {
+           if (allSockets[i]?.room == currentUserRoom) {
+             allSockets[i]?.socket.send(parsedMessage.payload.message);
+           }
+         }
+        }
+
+       
     })
-})
+
+
+   
+    })
